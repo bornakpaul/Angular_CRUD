dafd8d0a8ec028696ba75e289aa319a12c91f8ec
Bornak <bornakpaul@gmail.com>
2021-10-25 01:42:12 +0530

    complete

--- src/app/user-dashboard/user-dashboard.component.ts
+++ src/app/user-dashboard/user-dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ApiService } from '../shared/api.service';
+import { UserModel } from './user-dashboard.model';
 
 @Component({
   selector: 'app-user-dashboard',
@@ -7,9 +10,93 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDashboardComponent implements OnInit {
 
-  constructor() { }
+  formValue!: FormGroup;
+  userModelObj : UserModel = new UserModel();
+  userData !: any;
+  showAdd! : boolean;
+  showUpdate! : boolean;
+
+  constructor(private formbuilder: FormBuilder, private api: ApiService) { }
 
   ngOnInit(): void {
+    this.formValue = this.formbuilder.group({
+      firstName : [''],
+      lastName : [''],
+      email : [''],
+      mobile : [''],
+      password : ['']
+    })
+    this.getAllUsers();
+  }
+
+  clickAddUser(){
+    this.formValue.reset();
+    this.showAdd = true;
+    this.showUpdate = false;
+  }
+
+  postUserDetails(){
+    this.userModelObj.firstName = this.formValue.value.firstName;
+    this.userModelObj.lastName = this.formValue.value.lastName;
+    this.userModelObj.email = this.formValue.value.email;
+    this.userModelObj.mobile = this.formValue.value.mobile;
+    this.userModelObj.password = this.formValue.value.password;
+
+    this.api.postUser(this.userModelObj)
+    .subscribe(res=>{
+      console.log(res);
+      alert("User added successfully");
+      let ref = document.getElementById('cancel');
+      ref?.click();
+      this.formValue.reset();
+      this.getAllUsers();
+    },
+    err=>{
+      alert("Something went wrong")
+    })
+  }
+
+  getAllUsers(){
+    this.api.getUser()
+    .subscribe(res=>{
+      this.userData = res;
+    })
+  }
+
+  deleteUsers(row : any){
+    this.api.deleteUser(row.id)
+    .subscribe(res=>{
+      alert("User deleted");
+      this.getAllUsers();
+    })
+  }
+
+  onEdit(row: any){
+    this.showAdd = false;
+    this.showUpdate = true;
+    this.userModelObj.id = row.id;
+    this.formValue.controls['firstName'].setValue(row.firstName);
+    this.formValue.controls['lastName'].setValue(row.lastName);
+    this.formValue.controls['email'].setValue(row.email);
+    this.formValue.controls['mobile'].setValue(row.mobile);
+    this.formValue.controls['firstpassword'].setValue(row.password);
+  }
+
+  updateUserDetails(){
+    this.userModelObj.firstName = this.formValue.value.firstName;
+    this.userModelObj.lastName = this.formValue.value.lastName;
+    this.userModelObj.email = this.formValue.value.email;
+    this.userModelObj.mobile = this.formValue.value.mobile;
+    this.userModelObj.password = this.formValue.value.password;
+
+    this.api.updateUser(this.userModelObj,this.userModelObj.id)
+    .subscribe(res=>{
+      alert("Updated Successfully");
+      let ref = document.getElementById('cancel');
+      ref?.click();
+      this.formValue.reset();
+      this.getAllUsers();
+    })
   }
 
 }
